360ec99e874d3badd25c07302e877fdb1f0dc715
6erik <estanton@csumb.edu>
2020-12-07 20:03:57 -0800

    Added button to submit dice value selections to array

--- diary.py
+++ diary.py
@@ -5,17 +5,10 @@ HEIGHT = 400
 WIDTH = 400
 
 # BACKEND LOGIC STUFF
-class Die:
-    def __init__(self):
-        self.value = random.randrange(1,6)
-
-    def getValue(self):
-        return self.value
-
 class Roll:
-    def __init__(self):
-        self.die1 = Die()
-        self.die2 = Die()
+    def __init__(self, die1, die2):
+        self.die1 = die1
+        self.die2 = die2
 
     def getDie1value(self):
         return self.die1.getValue()
@@ -26,23 +19,58 @@ class Roll:
     def getCombinedValue(self):
         return self.getDie1value() + self.getDie2value()
 
-def rollDice():
-    roll = Roll()
+def submitDice():
+    d1 = die1value.get()
+    d2 = die2value.get()
+    roll = Roll(d1, d2)
     array_game.append(roll)
-    # print(array_game)
-
 
 array_session = []
 array_game = []
 
-
 # GUI STUFF
 root = tk.Tk()
 
-frame = tk.Frame()
-frame.pack()
+frame_main = tk.Frame()
+frame_main.pack()
 
-btnRoll = tk.Button(frame, text="Button", command=rollDice)
-btnRoll.pack()
+frame_die1 = tk.Frame(frame_main)
+frame_die1.grid(row=0, column=0)
+frame_die2 = tk.Frame(frame_main)
+frame_die2.grid(row=0, column=1)
+frame_submit = tk.Frame(frame_main)
+frame_submit.grid(row=1)
+
+die1value = tk.IntVar()
+die2value = tk.IntVar()
+
+d1r1 = tk.Radiobutton(frame_die1, text="1", variable=die1value, value=1)
+d1r1.pack()
+d1r2 = tk.Radiobutton(frame_die1, text="2", variable=die1value, value=2)
+d1r2.pack()
+d1r3 = tk.Radiobutton(frame_die1, text="3", variable=die1value, value=3)
+d1r3.pack()
+d1r4 = tk.Radiobutton(frame_die1, text="4", variable=die1value, value=4)
+d1r4.pack()
+d1r5 = tk.Radiobutton(frame_die1, text="5", variable=die1value, value=5)
+d1r5.pack()
+d1r6 = tk.Radiobutton(frame_die1, text="6", variable=die1value, value=6)
+d1r6.pack()
+
+d2r1 = tk.Radiobutton(frame_die2, text="1", variable=die2value, value=1)
+d2r1.pack()
+d2r2 = tk.Radiobutton(frame_die2, text="2", variable=die2value, value=2)
+d2r2.pack()
+d2r3 = tk.Radiobutton(frame_die2, text="3", variable=die2value, value=3)
+d2r3.pack()
+d2r4 = tk.Radiobutton(frame_die2, text="4", variable=die2value, value=4)
+d2r4.pack()
+d2r5 = tk.Radiobutton(frame_die2, text="5", variable=die2value, value=5)
+d2r5.pack()
+d2r6 = tk.Radiobutton(frame_die2, text="6", variable=die2value, value=6)
+d2r6.pack()
+
+button_submit = tk.Button(frame_submit, text="Submit", command=submitDice)
+button_submit.pack()
 
 root.mainloop()
