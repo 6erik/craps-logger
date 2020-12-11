f6bbaa56a389799449fa1aad781587890e984fe3
6erik <estanton@csumb.edu>
2020-12-10 21:39:12 -0800

    Added class module files to transition to OOP

---
+++ session.py
@@ -0,0 +1,13 @@
+# Session class module
+from game import Game
+
+class Session:
+    def __init__(self):
+        self.array_games = []
+
+    def add_game(self, game):
+        if type(game) is Game:
+            self.array_games.append(game)
+
+        else:
+            raise TypeError("Object must be of type 'Game'")
